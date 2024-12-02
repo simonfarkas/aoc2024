9b5e9d3f717974dd4f7708c29e22ded193f6dccf
Simon Farkas <simonfarkasdev@gmail.com>
2024-12-02 18:50:34 +0100

    Day 2 - missing ts types

--- day2/part1.ts
+++ day2/part1.ts
@@ -1,40 +1,44 @@
 import * as fs from 'fs';
 
-const rows = fs.readFileSync('input.txt', 'utf8')
+const rows: string[] = fs.readFileSync('input.txt', 'utf8')
     .split('\n')
     .filter(Boolean);
 
 let total = 0;
 
-const isIncreasing = (arr: number[]) => {
-	return arr.filter((num) => num > 0)
-	  .every((num, idx, arr) => {
-	    if (idx === 0) return true;
-	    return num > arr[idx - 1];
- 	});	
+const isIncreasing = (arr: number[]): boolean => {
+    return arr.filter((num) => num > 0)
+        .every((num, idx, arr) => {
+            if (idx === 0) return true;
+            return num > arr[idx - 1];
+        });
 }
 
-const isDecreasing = (arr: number[]) => {
-	return arr.filter((num) => num > 0)
-	  .every((num, idx, arr) => {
-	    if (idx === 0) return true;
-	    return num < arr[idx - 1];
- 	});	
+const isDecreasing = (arr: number[]): boolean => {
+    return arr.filter((num) => num > 0)
+        .every((num, idx, arr) => {
+            if (idx === 0) return true;
+            return num < arr[idx - 1];
+        });
 }
 
-for(let i = 0; i < rows.length; i++) {
-	const row = rows[i].split(" ").map(n => Number(n))
-	let isRowValid = true;
-	for(let j = 0; j < row.length - 1; j++) {
-		const diff = Math.abs(row[j] - row[j+1])
-		if(diff > 3 || diff < 1) {
-			isRowValid = false;
-			break;
-		}
-	}
-	const increasing = isIncreasing(row)
-	const decreasing = isDecreasing(row)
-	if(isRowValid && (increasing || decreasing)) total++;
+for (let i = 0; i < rows.length; i++) {
+    const row: number[] = rows[i].split(" ").map(n => Number(n));
+
+    let isRowValid: boolean = true;
+
+    for (let j = 0; j < row.length - 1; j++) {
+        const diff: number = Math.abs(row[j] - row[j + 1]);
+        if (diff > 3 || diff < 1) {
+            isRowValid = false;
+            break;
+        }
+    }
+
+    const increasing: boolean = isIncreasing(row);
+    const decreasing: boolean = isDecreasing(row);
+
+    if (isRowValid && (increasing || decreasing)) total++;
 }
 
-console.log(total)
+console.log(total);
--- day2/part2.ts
+++ day2/part2.ts
@@ -1,62 +1,68 @@
 import * as fs from 'fs';
 
-const rows = fs.readFileSync('input.txt', 'utf8')
+const rows: string[] = fs.readFileSync('input.txt', 'utf8')
     .split('\n')
     .filter(Boolean);
 
 let total = 0;
 
-const isIncreasing = (arr: number[]) => {
-	return arr.filter((num) => num > 0)
-	  .every((num, idx, arr) => {
-	    if (idx === 0) return true;
-	    return num > arr[idx - 1];
- 	});	
+const isIncreasing = (arr: number[]): boolean => {
+    return arr.filter((num) => num > 0)
+        .every((num, idx, arr) => {
+            if (idx === 0) return true;
+            return num > arr[idx - 1];
+        });
 }
 
-const isDecreasing = (arr: number[]) => {
-	return arr.filter((num) => num > 0)
-	  .every((num, idx, arr) => {
-	    if (idx === 0) return true;
-	    return num < arr[idx - 1];
- 	});	
+const isDecreasing = (arr: number[]): boolean => {
+    return arr.filter((num) => num > 0)
+        .every((num, idx, arr) => {
+            if (idx === 0) return true;
+            return num < arr[idx - 1];
+        });
 }
 
-const isRowSafeAfterRemove = (arr: number[]) => {
-	let i = 0;
-	while(i < arr.length - 2) {
-		if(arr[i] < arr[i+1] && arr[i+1] > arr[i+2]) {
-			arr.splice(i+1, 1)
-			if(i > 0) i--
-		}
-		if(arr[i] > arr[i+1] && arr[i+1] < arr[i+2]) {
-			arr.splice(i+1, 1)
-			if(i > 0) i--
-		}
-		if(arr[i] === arr[i+1]) {
-			arr.splice(i+1, 1)
-			if(i > 0) i--
-		}
-		i++
-	}
-	return arr;
+const isRowSafeAfterRemove = (arr: number[]): number[] => {
+    let i = 0;
+    while (i < arr.length - 2) {
+        if (arr[i] < arr[i + 1] && arr[i + 1] > arr[i + 2]) {
+            arr.splice(i + 1, 1);
+            if (i > 0) i--;
+        }
+        if (arr[i] > arr[i + 1] && arr[i + 1] < arr[i + 2]) {
+            arr.splice(i + 1, 1);
+            if (i > 0) i--;
+        }
+        if (arr[i] === arr[i + 1]) {
+            arr.splice(i + 1, 1);
+            if (i > 0) i--;
+        }
+        i++;
+    }
+    return arr;
 }
 
-for(let i = 0; i < rows.length; i++) {
-	const row = rows[i].split(" ").map(n => Number(n))
-	let isRowValid = true;
-	const safeRow = isRowSafeAfterRemove(row)
-	for(let j = 0; j < safeRow.length - 1; j++) {
-		const diff = Math.abs(safeRow[j] - safeRow[j+1])
-		if(diff > 3 || diff < 1) {
-			isRowValid = false;
-			break;
-		}
-	}
-	const increasing = isIncreasing(safeRow)
-	const decreasing = isDecreasing(safeRow)
-	console.log(`Row: ${safeRow}, the diff is ${isRowValid ? 'correct' : 'incorrect'}, ${increasing ? 'increasing' : "decreasing"	 }`)
-	if(isRowValid && (increasing || decreasing)) total++;
+for (let i = 0; i < rows.length; i++) {
+    const row: number[] = rows[i].split(" ").map(n => Number(n));
+
+    let isRowValid: boolean = true;
+
+    const safeRow: number[] = isRowSafeAfterRemove(row);
+
+    for (let j = 0; j < safeRow.length - 1; j++) {
+        const diff: number = Math.abs(safeRow[j] - safeRow[j + 1]);
+        if (diff > 3 || diff < 1) {
+            isRowValid = false;
+            break;
+        }
+    }
+
+    const increasing: boolean = isIncreasing(safeRow);
+    const decreasing: boolean = isDecreasing(safeRow);
+
+    console.log(`Row: ${safeRow}, the diff is ${isRowValid ? 'correct' : 'incorrect'}, ${increasing ? 'increasing' : "decreasing"}`);
+
+    if (isRowValid && (increasing || decreasing)) total++;
 }
 
-console.log(total)
+console.log(total);
